6a1c1e56e0a99a42f568bb2541de31b634a3db59
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-02-29 08:04:31 +0600

    Update Badol1.js

--- scripts/commands/Badol1.js
+++ scripts/commands/Badol1.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 module.exports.config = {
-	    name: "😳",
+	    name: "🥺",
     version: "1.1.0",
     permission: 0,
     credits: "BADOL-KHAN",
     description: "noprefix",
     prefix: true,
     category: "commands",
-    usages: "😳",
+    usages: "🥺",
     cooldowns: 5,
 };
 
