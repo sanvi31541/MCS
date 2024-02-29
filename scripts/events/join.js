fecef2f4ca2ab4a2e1595f386c349c5bbaff4c7b
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-02-29 08:35:08 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,8 +48,8 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://i.imgur.com/K7xo8C8.mp4';
-let gifPath = __dirname + '/Nayan/join/join.mp4';
+    let gifUrl = 'https://i.postimg.cc/SNQXkB0y/lv-0-20231018174834.gif';
+let gifPath = __dirname + '/Nayan/join/join.gif';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
 .then(response => {
